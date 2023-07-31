ab87dd4ba9e5075bd7fec5c84a1b9fb9324271a4
Uniquearena <119504911+Uniquearena@users.noreply.github.com>
2023-07-31 18:30:43 +0530

    Add files via upload

---
+++ Basics of python2.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[2]:
+
+
+Print(data)
+
+
+# In[3]:
+
+
+print('data')
+
+
+# In[4]:
+
+
+print("data")
+
+
+# In[5]:
+
+
+print(10.7)
+
+
+# In[6]:
+
+
+print(10)
+
+
+# ### fefugyfbyef
+
+# ### Variables
+
+# In[7]:
+
+
+first name = "Prema"
+
+
+# In[8]:
+
+
+Firstname = "Prema"
+
+
+# In[9]:
+
+
+Firstname = "Prema"
+firstname
+
+
+# In[10]:
+
+
+name = "data"
+name
+
+
+# In[11]:
+
+
+1name = "Latha"
+1name
+
+
+# In[12]:
+
+
+name1 = "latha"
+name1
+
+
+# In[13]:
+
+
+first+name = "artificial"
+first+name
+
+
+# In[14]:
+
+
+first_name = "artificial"
+first_name
+
+
+# In[15]:
+
+
+type(first_name)
+
+
+# In[16]:
+
+
+x = 10.5
+type(x)
+
+
+# In[17]:
+
+
+z = "true"
+float(z)
+
+
+# In[18]:
+
+
+z = "ansh"
+type(z)
+
+
+# In[19]:
+
+
+x = 10
+type(x)
+x
+
+
+# In[20]:
+
+
+x = 10
+x,type(x)
+
+
+# In[22]:
+
+
+x,y = 10,20
+x,y
+
+
+# In[28]:
+
+
+a = "Ansh"
+print(a)
+type(a)
+
+
+# In[29]:
+
+
+b = 20
+print(b)
+type(b)
+
+
+# In[ ]:
+
+
+
+
