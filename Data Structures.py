7b5b27b1f39ede91d7f43fe84aa7f626716ec51a
Uniquearena <119504911+Uniquearena@users.noreply.github.com>
2023-07-31 19:00:34 +0530

    Add files via upload

---
+++ Data Structures.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Data Structures
+
+# In[1]:
+
+
+sample_list = [1,13.6,"hi",12,1]
+sample_list
+
+
+# In[2]:
+
+
+type(sample_list)
+
+
+# In[6]:
+
+
+sample_list[5]
+
+
+# 
+
+# In[7]:
+
+
+sample_list[-1]
+
+
+# In[8]:
+
+
+sample_list[0] = 10
+
+
+# In[9]:
+
+
+sample_list
+
+
+# ### Tuple
+
+# In[10]:
+
+
+sample_tuple = (1,13.6,"hi",12,1)
+sample_tuple
+
+
+# In[11]:
+
+
+type(sample_tuple)
+
+
+# In[15]:
+
+
+sample_tuple[10]
+
+
+# In[16]:
+
+
+sample_tuple[0] = 10
+
+
+# ### Set
+
+# In[17]:
+
+
+sample_set = (1,13.6,"hi",12,1,127,25,25,"apple","king","ball")
+sample_set
+
+
+# In[19]:
+
+
+sample_set[8]
+
+
+# In[20]:
+
+
+type(sample_set[8])
+
+
+# In[21]:
+
+
+sample_set[0] = "hello"
+sample_set
+
+
+# In[22]:
+
+
+sample_set.add(1000)
+
+
+# ### Dictionary
+
+# 
+
+# In[27]:
+
+
+sample_dict = {1:"apple",2:35,3:"data",2:"every",6:35,7:35.4,2:"hi"}
+sample_dict
+
+
+# In[24]:
+
+
+type(sample_set)
+
+
+# In[29]:
+
+
+sample_dict[3]
+
+
+# In[28]:
+
+
+sample_dict["apple"]
+
+
+# In[31]:
+
+
+sample_dict[7] = 100
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
